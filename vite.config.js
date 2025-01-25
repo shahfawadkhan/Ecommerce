473a58c230f6d6e184d2ab2392b36be07b664509
Shah Fawad Khan <155229733+shahfawadkhan@users.noreply.github.com>
2025-01-25 12:21:23 +0500

    install packages for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'Ecommerce' 
-})
+  plugins: [react(), ghPages()],
+  base: 'Ecommerce', // Replace with your GitHub repo name
+});
